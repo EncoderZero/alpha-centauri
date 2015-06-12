fd0ecdedb446dd6574b21dde2d065e9cabdb65b1
Kevin <intrepidhonor@gmail.com>
2015-06-11 22:24:01 -0400

    setup framework

---
+++ binbash/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(binbash.Startup))]
+namespace binbash
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
