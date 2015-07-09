10fa34048f1e6d9282533f22d25b451f116b2537
Welch1914 <sw_welch@hotmail.com>
2015-07-09 10:28:11 -0400

    Add Cart Controller

---
+++ binbash/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace binbash.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Shopping
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
